a6a2a8ba2fbe5f33f525997c1285127f1ce6c3a0
Merima Dedic <merimaxdedic@gmail.com>
2023-11-18 19:21:39 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    const matchingDrivers = [];
+  
+    for (let driver of drivers) {
+      if (driver.toLowerCase().includes(name.toLowerCase())) {
+        matchingDrivers.push(driver);
+      }
+    }
+  
+    return matchingDrivers
+  }
+
+  function fuzzyMatch(drivers, letters) {
+    const matchingDrivers = [];
+  
+    for (let driver of drivers) {
+      if (driver.startsWith(letters)) {
+        matchingDrivers.push(driver);
+      }
+    }
+  
+    return matchingDrivers;
+  }
+
+  function matchName(drivers, name) {
+    const matchingDrivers = [];
+  
+    for (let driver of drivers) {
+      if (driver.name === name) {
+        matchingDrivers.push(driver);
+      }
+    }
+  
+    return matchingDrivers;
+  }
